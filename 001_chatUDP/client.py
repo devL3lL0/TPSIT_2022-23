f7f98cb991ce4cc43613c1502bd300e1919f6205
Leonardo Musso <93180266+LeoMusso@users.noreply.github.com>
2022-09-13 15:58:00 +0200

    Update client.py

--- 001_chatUDP/client.py
+++ 001_chatUDP/client.py
@@ -1,10 +1,20 @@
 from socket import AF_INET, SOCK_DGRAM, socket 
 
-IP = "192.168.95.255"
 PORT = 5000
+
+def ip_locale():
+    s = socket(AF_INET, SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_loc = s.getsockname()[0]
+    print("ip locale server: ", ip_loc)
+    s.close()
+    return ip_loc
 
 def mainChatClient():
     running = True
+    ip_loc = ip_locale()
+    ip_loc = ip_loc.split('.')
+    ip_broadcast = ip_loc[0] + '.' + ip_loc[1] + '.' + ip_loc[2] + '.' + '255'
     with socket(AF_INET, SOCK_DGRAM) as s:
         while running == True:
             mex = input("Inserire il messaggio: ")
@@ -12,7 +22,7 @@ def mainChatClient():
                 running = False
             else:
                 mex = mex.encode()
-                s.sendto(mex, (IP, PORT))
+                s.sendto(mex, (ip_broadcast, PORT))
 
 if __name__ == "__main__":
     mainChatClient()
