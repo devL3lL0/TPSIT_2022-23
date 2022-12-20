03f7b4e34241a9758ee82cfc8f1a43e03f9c0be4
Leonardo Musso <leonardo.musso@itiscuneo.eu>
2022-12-20 08:57:47 +0100

    chat robot debuggata

--- 003_chatRobot/client.py
+++ 003_chatRobot/client.py
@@ -1,20 +1,20 @@
 from socket import AF_INET, SOCK_STREAM, socket
 
 def chatClient():
-    addres = str(input("Inserire l'ip del robot: "))
+    address = str(input("Inserire l'ip del robot: "))
     with socket(AF_INET, SOCK_STREAM) as s:
-        address = (addres, 3450)
-        print(address)
+        address = (address, 3450)
         running = True
         s.connect(address)
+        robot = input("Robot reale o virtuale?: ")
+        s.send(robot.upper().encode('utf8'))
         while running == True:
             msg = str(input("Inserire il comando: "))
             msg = msg.upper()
             if msg == 'E':
                 running = False
-            else:
-                s.send(msg.encode('utf8'))
-
-
+            s.send(msg.encode('utf8'))
+        s.close()
+    print("Chiusura client effettuata")
 if __name__ == "__main__":
     chatClient()
--- 003_chatRobot/serverRobot.py
+++ 003_chatRobot/serverRobot.py
@@ -1,37 +1,94 @@
-from socket import AF_INET, SOCK_STREAM, socket
+from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, socket
 import time
-from AlphaBot import AlphaBot
+#from AlphaBot import AlphaBot
+from AlphaBotDebug import AlphaBotDebug
+
 
 def chatServer():
-    ab = AlphaBot()
+    quest = input("Robot Virtuale o Reale?: ")
+    if quest.upper() == 'REALE':
+        #ab = AlphaBot()
+        pass
+    else:
+        ab = AlphaBotDebug()
+        print("Creata istanza virtuale")
+
+    ab = AlphaBotDebug()
     ab.stop()
     with socket(AF_INET, SOCK_STREAM) as s:
-    
+        s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         s.bind(("0.0.0.0", 3450))
-        
-        
+
+        running = True
 
         s.listen()
+
         print("In Ascolto")
-        
+
         client, clientAddress = s.accept()
-        
+
+        move = []
+        rec = False
         ab.stop()
-        while ab:
-            ab.stop()
-            
+        while ab and running == True:
             msg = client.recv(1024).decode('utf8')
             msg = msg.upper()
-                
+            while rec == True:
+                if rec == True:
+                    if msg == 'W' or msg == 'B' or msg == 'L' or msg == 'S':
+                        print(f"Sto registrando il movimento {msg}")
+                        move.append(msg)
+                    elif msg == 'STOP':
+                        rec = False
+                    else:
+                        print("Movimento non supportato")
+            if msg == 'REC':
+                print("In")
+                rec = True
+            if msg == 'STOP':
+                print("Inizio a muovermi")
+                for movement in move:
+                    print(movement)
+                    if movement == 'W':
+                        ab.forward()
+                    elif movement == 'R':
+                        ab.right()
+                    elif movement == 'L':
+                        ab.left()
+                    elif movement == 'B':
+                        ab.backward()
+                    else:
+                        print("Movimento non supportato")
             if msg == 'W':
                 print('sto andando avanti')
                 ab.forward()
                 time.sleep(3)
-            elif msg == 'D':
+
+            elif msg == 'R':
                 ab.stop()
                 ab.right()
                 time.sleep(3)
+
+            elif msg == 'L':
+                ab.stop()
+                ab.left()
+                time.sleep(3)
+
+            elif msg == 'B':
+                ab.stop()
+                ab.backward()
+                time.sleep(3)
+
+            elif msg == 'E':
+                print("Uscita dall'ascolto in corso")
+                running = False
+
+            else:
+                print("Comando non supportato")
             ab.stop()
+
+        s.close()
+    print("Chiusura server effettuata")
 
 
 if __name__ == "__main__":
